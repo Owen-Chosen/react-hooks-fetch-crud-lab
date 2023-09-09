3ed36f36f57887ec70a83e8eab2117c8859ca8c9
owen <owen.chosen@gmail.com>
2023-09-09 11:52:18 +0100

    Done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, callSetPage, questionNum }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,15 +9,39 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  function handleDeleteButton (event) {
+    fetch(`http://localhost:4000/questions/${event.target.id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
+    }).then(res => res.json())
+    .then(data => callSetPage())
+  }
+
+  function handleChange (event) {
+    console.log(event.target.value)
+    fetch(`http://localhost:4000/questions/${event.target.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({ correctIndex: parseInt(event.target.value, 10) })
+    }).then(res => res.json())
+    .then(data => {callSetPage(); console.log(data); callSetPage()})
+  }
+
   return (
     <li>
-      <h4>Question {id}</h4>
+      <h4>Question {questionNum}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select onChange={handleChange} id={id} defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button id={id} onClick={handleDeleteButton}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,23 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ setPage, questions}) {
+
+  let count = 1;
+
+  function callSetPage () {
+    setPage("Form")
+    setPage("List")
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questions.map((question) => {
+        return <QuestionItem callSetPage={callSetPage}
+         key={question.id} questionNum={count++}
+          question={question}/>
+      })}</ul>
     </section>
   );
 }
